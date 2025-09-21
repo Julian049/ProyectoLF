70922d73430f6689f1fd6731778c680bb0601a11
Julian <julianb0904@gmail.com>
2025-09-20 22:34:36 -0500

    Create basic options pane

--- src/co/edu/uptc/view/ManagerView.java
+++ src/co/edu/uptc/view/ManagerView.java
@@ -2,9 +2,18 @@ package co.edu.uptc.view;
 
 import co.edu.uptc.presenter.ContractMVP;
 
-public class ManagerView implements ContractMVP.View {
+import javax.swing.*;
+import javax.swing.text.html.Option;
+
+public class ManagerView extends JFrame implements ContractMVP.View {
 
     private ContractMVP.Presenter presenter;
+    private OptionsPanel optionsPanel;
+
+    public ManagerView() {
+        createFrame();
+        optionsPanel = new OptionsPanel();
+    }
 
     @Override
     public void setPresenter(ContractMVP.Presenter presenter) {
@@ -13,6 +22,16 @@ public class ManagerView implements ContractMVP.View {
 
     @Override
     public void initUI() {
+        this.add(optionsPanel);
+        setVisible(true);
+    }
 
+    private void createFrame() {
+        setTitle("Simulador de automatas deterministas");
+        setSize(800, 800);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(false);
     }
 }
---
+++ src/co/edu/uptc/view/OptionsPanel.java
@@ -0,0 +1,199 @@
+package co.edu.uptc.view;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class OptionsPanel extends JPanel {
+
+    private JLabel statesTitle;
+    private JTextField statesValues;
+    private JButton addStatesButton;
+    private JLabel symbolsTitle;
+    private JTextField symbolsValue;
+    private JButton addSymbolsButton;
+    private JLabel initialStateTitle;
+    private JTextField initialStateValue;
+    private JButton initialStateButton;
+    private JLabel finalStateTitle;
+    private JTextField finalStateValue;
+    private JButton finalStateButton;
+    private JTable transitionsTable;
+    private DefaultTableModel tableModel;
+    private ArrayList<String> columnNames = new ArrayList<>();
+    private ArrayList<String> rowValues = new ArrayList<>();
+
+    public OptionsPanel() {
+        this.setLayout(new GridBagLayout());
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        createStatesTitle();
+        createStatesValues();
+        createAddStatesButton();
+        createSymbolsTitle();
+        createSymbolsValue();
+        createAddSymbolsButton();
+        createInitialStateTitle();
+        createInitialStateValue();
+        createInitialStateButton();
+        createFinalStateTitle();
+        createFinalStateValue();
+        createFinalStateButton();
+        createTransitionsTable();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        this.add(statesTitle, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        this.add(statesValues, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        this.add(addStatesButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        this.add(symbolsTitle, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        this.add(symbolsValue, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        this.add(addSymbolsButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        this.add(initialStateTitle, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.gridwidth = 1;
+        this.add(initialStateValue, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        gbc.gridwidth = 1;
+        this.add(initialStateButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        gbc.gridwidth = 2;
+        this.add(finalStateTitle, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        gbc.gridwidth = 1;
+        this.add(finalStateValue, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 7;
+        gbc.gridwidth = 1;
+        this.add(finalStateButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        gbc.fill = GridBagConstraints.BOTH;
+        JScrollPane scrollPane = new JScrollPane(transitionsTable);
+        this.add(scrollPane, gbc);
+    }
+
+
+    private void createStatesTitle() {
+        statesTitle = new JLabel("Agregue los estados separados por comas (q0,q1,q2)");
+    }
+
+    private void createStatesValues() {
+        statesValues = new JTextField();
+        statesValues.setEditable(true);
+        statesValues.setColumns(10);
+    }
+
+    private void createAddStatesButton() {
+        addStatesButton = new JButton("Agregar");
+        addStatesButton.addActionListener(e -> {
+            columnNames.add(statesValues.getText());
+            tableModel.addColumn(statesValues.getText());
+            revalidate();
+        });
+    }
+
+    private void createSymbolsTitle() {
+        symbolsTitle = new JLabel("Agregue los simbolos para el automata separados por comas (a,b,c)");
+    }
+
+    private void createSymbolsValue() {
+        symbolsValue = new JTextField();
+        symbolsValue.setEditable(true);
+        symbolsValue.setColumns(10);
+    }
+
+    private void createAddSymbolsButton() {
+        addSymbolsButton = new JButton("Agregar");
+        addSymbolsButton.addActionListener(e -> {
+            System.out.println(symbolsValue.getText());
+            rowValues.add(symbolsValue.getText());
+            tableModel.addRow(rowValues.toArray());
+        });
+    }
+
+    private void createInitialStateTitle() {
+        initialStateTitle = new JLabel("Ingrese el estado incial");
+    }
+
+    private void createInitialStateValue() {
+        initialStateValue = new JTextField();
+        initialStateValue.setEditable(true);
+        initialStateValue.setColumns(10);
+    }
+
+    private void createInitialStateButton() {
+        initialStateButton = new JButton("Agregar");
+        initialStateButton.addActionListener(e -> {
+            System.out.println(initialStateValue.getText());
+        });
+    }
+
+    private void createFinalStateTitle() {
+        finalStateTitle = new JLabel("Seleccione el estado final, en caso de ser varios separados por comas (q0,q1,q2)");
+
+    }
+
+    private void createFinalStateValue() {
+        finalStateValue = new JTextField();
+        finalStateValue.setEditable(true);
+        finalStateValue.setColumns(10);
+    }
+
+    private void createFinalStateButton() {
+        finalStateButton = new JButton("Agregar");
+        finalStateButton.addActionListener(e -> {
+            System.out.println(finalStateValue.getText());
+        });
+    }
+
+    private void createTransitionsTable() {
+        columnNames.add("estado");
+        rowValues.add(" ");
+        tableModel = new DefaultTableModel(columnNames.toArray(), 0);
+        transitionsTable = new JTable(tableModel);
+
+
+    }
+}
